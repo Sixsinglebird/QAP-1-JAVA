83ae313a7b2cb8810ce363d6f5566bc638d9a1c5
Alex Singleton <alex.singleton@keyin.com>
2023-05-19 17:40:00 -0230

    Date.java & accompanying test file.

--- Date/Date.java
+++ Date/Date.java
@@ -1,5 +1,74 @@
 package Date;
 
 public class Date {
-    
+
+  private int day;
+  private int month;
+  private int year;
+  private String date = "";
+
+  public Date(int day, int month, int year) {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
+
+  public int getDay() {
+    return day;
+  }
+
+  public int getMonth() {
+    return month;
+  }
+
+  public int getYear() {
+    return year;
+  }
+
+  public void setDay(int day) {
+    this.day = day;
+  }
+
+  public void setMonth(int month) {
+    this.month = month;
+  }
+
+  public void setYear(int year) {
+    this.year = year;
+  }
+
+  public void setDate(int day, int month, int year) {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
+
+  public String fixDate() {
+    fixDay();
+    fixMonth();
+    this.date += this.year;
+    return this.date;
+  }
+
+  public void fixDay() {
+    if (this.day < 10) {
+      this.date += "0" + this.day + "/";
+    } else {
+      this.date += this.day + "/";
+    }
+  }
+
+  public void fixMonth() {
+    if (this.month < 10) {
+      this.date += "0" + this.month + "/";
+    } else {
+      this.date += this.month + "/";
+    }
+  }
+
+  @Override
+  public String toString() {
+    // TODO Auto-generated method stub
+    return fixDate();
+  }
 }
--- Date/testDate.java
+++ Date/testDate.java
@@ -2,5 +2,9 @@ package Date;
 
 public class testDate {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    Date birthDay = new Date(24, 5, 2023);
+
+    System.out.println(birthDay);
+  }
 }
