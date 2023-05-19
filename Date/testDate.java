8a21874bdb8d87db5640efe4e0b745ab59c01ecb
Alex Singleton <alex.singleton@keyin.com>
2023-05-19 17:41:47 -0230

    commented date

--- Date/testDate.java
+++ Date/testDate.java
@@ -3,6 +3,7 @@ package Date;
 public class testDate {
 
   public static void main(String[] args) {
+    // my birthday.... not the queen. : )
     Date birthDay = new Date(24, 5, 2023);
 
     System.out.println(birthDay);
